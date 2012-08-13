3663d6067849b0124a09fbc099dd2b9cdd38afb7
owainlewis <owain@owainlewis.com>
2012-08-13 20:32:45 +0100

    added basic math fns

--- src/variance/core.clj
+++ src/variance/core.clj
@@ -1,12 +1,14 @@
 (ns variance.core
   {:doc "A collection of useful math/stat functions"})
 
-(defn mean [values]
+(defn mean
+  {:doc "Return the mean for a set of values"}
+  [values]
   (float
     (/ (reduce + values) (count values))))
 
 (defn median
-  "Returns the median value for a sequence or pair of numbers"
+  {:doc "Returns the median value for a sequence or pair of numbers"}
   ([data]
     (let [sorted (sort data)
         count (count data)
@@ -19,9 +21,13 @@
        (median r))))
 
 (def sqrt (fn [x] (Math/sqrt x)))
+(def sin (fn [x] (Math/sin x)))
+(def cos (fn [x] (Math/cos x)))
+(def tan (fn [x] (Math/tan x)))
 
 (defn variance
-  "Returns the variance for a collection of values"
+  {:doc "Returns the variance for a collection of values.
+   A measure of how far a set of numbers is spread out"}
   [values]
   (def sqr (fn [x] (* x x)))
   (let [mv (mean values)]
@@ -31,6 +37,13 @@
           (count values))))
   
 (defn standard-deviation [values]
+  {:doc "In statistics and probability theory, standard deviation (represented by the symbol sigma, σ)
+   shows how much variation or dispersion exists from the average (mean, or expected value).
+   A low standard deviation indicates that the data points tend to be very close to the mean,
+   whereas high standard deviation indicates that the data points are spread out over a large
+   range of values."}
   (sqrt (variance values)))
 
 (defn covariance [data])
+
+(defn gini-coefficient [])
