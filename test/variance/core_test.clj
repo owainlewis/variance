ff80f4c8c71667e1ce2cd4376d363a2bbd326160
owainlewis <owain@owainlewis.com>
2012-08-24 23:20:00 +0100

    added tests for dot product

--- test/variance/core_test.clj
+++ test/variance/core_test.clj
@@ -1,23 +1,35 @@
 (ns variance.core-test
   (:use midje.sweet
+        clojure.test
         variance.core))
 
+(deftest dot-product-test
+(facts "about dot product"
+  (dot-p [1 3 -5] [4 -2 -1]) => 3
+  (dot-product (double-array [1 3 -5])
+             (double-array [4 -2 -1])) => 3.0))
+
+(deftest mean-value-test
 (facts "about mean values"
   (mean [3, 7, 5, 13, 20, 23, 39, 23, 40, 23, 14, 12, 56, 23, 29])
      => 22.0
   (mean [3, -7, 5, 13, -2])
-     => 2.4)
+     => 2.4))
 
+(deftest median-test
 (facts "about median values"
   (median 10 21) => 15
-  (median [8, 3, 44, 17, 12, 6]) => 10)
+  (median [8, 3, 44, 17, 12, 6]) => 10))
 
+(deftest range-test
 (facts "about rng"
-  (rng [3 6 6 6 7 9 11 11 13]) => 10)
+  (rng [3 6 6 6 7 9 11 11 13]) => 10))
 
+(deftest variance-test
 (facts "about variance"
-  (int (variance [600 470 170 430 300])) => 21704)
+  (int (variance [600 470 170 430 300])) => 21704))
 
+(deftest standard-deviation-test
 (facts "about standard-deviation"
   (int (standard-deviation [600 470 170 430 300]))
-    => 147)
+    => 147))
