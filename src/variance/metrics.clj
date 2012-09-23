23585f1aae575deab760fa759d2705ebf9516e62
owainlewis <owain@owainlewis.com>
2012-09-23 12:02:34 +0100

    tidy up incomplete functions

--- src/variance/metrics.clj
+++ src/variance/metrics.clj
@@ -22,16 +22,6 @@
   (let [[x1 x2] x [y1 y2] y]
     (+ (- x1 x2)) (- y1 y2)))
 
-(defn minkowski-distance [])
-
-(defn levenshtein-distance
-  "The Levenshtein distance between two strings is defined as the minimum number
-   of edits needed to transform one string into the other, with the allowable edit
-   operations being insertion, deletion, or substitution of a single character."
-  [^String s1 ^String s2]
-  (let [m (count s1)
-        n (count s2)]))
-
 (defn distance-function
   "Returns a memoized distance function. You specify the functions that determine insertion and
   replacement costs. Provided by Spencer Tipping at Factual"
@@ -49,3 +39,15 @@
                                           (+ (insertion-cost-fn a)     (distance a-rest bs)))))]
             (swap! memo-table assoc [as bs] result)
             result)))))
+
+;; TODO
+
+(defn minkowski-distance [])
+
+(defn levenshtein-distance
+  "The Levenshtein distance between two strings is defined as the minimum number
+   of edits needed to transform one string into the other, with the allowable edit
+   operations being insertion, deletion, or substitution of a single character."
+  [^String s1 ^String s2]
+  (let [m (count s1)
+        n (count s2)]))
